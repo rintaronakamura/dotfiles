2714cc4f1b0bd20443433da86711ff6ae9335f96
residenti <n.queue.r@gmail.com>
2019-01-10 14:33:51 +0900

    add .bash_profie & .bashrc

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
+# ln -s ~/dotfiles/_bash_profile ~/.bash_profile
+# ln -s ~/dotfiles/_bashrc ~/.bashrc
 
 ln -s ~/dotfiles/_vimrc ~/.vimrc
 ln -s ~/dotfiles/_dein.toml ~/dein.toml
 
 ln -s ~/dotfiles/_gitconfig ~/.gitconfig
 ln -s ~/dotfiles/_gitignore_global ~/.gitignore_global
-
-
