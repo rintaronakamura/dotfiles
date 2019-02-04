21a919400564a1f516e237970153f0c430114e28
residenti <n.queue.r@gmail.com>
2019-02-04 23:41:34 +0900

    add brew.sh

---
+++ brew.sh
@@ -0,0 +1,14 @@
+brew install git
+brew install ctags
+brew install curl
+brew install mysql
+brew install tree
+brew tap heroku/brew && brew install heroku
+
+brew tap caskroom/fonts
+brew cask install font-hack-nerd-font
+
+brew cask install iterm2
+brew cask install alfred
+brew cask install sblack
+brew cask install google-chrome
--- setup.sh
+++ setup.sh
@@ -7,3 +7,5 @@ ln -s ~/dotfiles/_dein.toml ~/dein.toml
 ln -s ~/dotfiles/_gitconfig ~/.gitconfig
 test ! -e ~/.config/git && mkdir ~/.config/git
 ln -s ~/dotfiles/.config/git/ignore ~/.config/git/ignore
+
+sh ./brew.sh
