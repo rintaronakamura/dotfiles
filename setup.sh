f53c2cfef0b3755a378fc8abce24ba22fe36d84c
residenti <n.queue.r@gmail.com>
2019-02-09 13:49:19 +0900

    add symbol link

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
-# ln -s ~/dotfiles/_bash_profile ~/.bash_profile
-# ln -s ~/dotfiles/_bashrc ~/.bashrc
-
-ln -s ~/dotfiles/_vimrc ~/.vimrc
-ln -s ~/dotfiles/_dein.toml ~/dein.toml
+ln -s ~/dotfiles/_bash_profile ~/.bash_profile
+ln -s ~/dotfiles/_bashrc ~/.bashrc
 
 ln -s ~/dotfiles/_gitconfig ~/.gitconfig
 test ! -e ~/.config/git && mkdir ~/.config/git
 ln -s ~/dotfiles/.config/git/ignore ~/.config/git/ignore
 
 sh ./brew.sh
+
+ln -s ~/dotfiles/_vimrc ~/.vimrc
+ln -s ~/dotfiles/_dein.toml ~/dein.toml
