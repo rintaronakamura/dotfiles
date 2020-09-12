38481bb4c4b8187cb358ae1e861ddf92a5b51198
residenti <n.queue.r@gmail.com>
2020-09-12 22:37:27 +0900

    weight saving for setup.

--- brew.sh
+++ brew.sh
@@ -1,17 +1,12 @@
 brew install tmux
 brew install openssl
 brew install curl
-brew install mysql
-brew install sqlite
-brew install postgresql
 brew install fzf
 brew install jq
 brew install bat
 brew install tree
 brew install the_silver_searcher
 brew install vim
-brew install ffmpeg
-brew install go
 brew install ghq
 brew install rbenv ruby-build
 brew tap heroku/brew && brew install heroku
