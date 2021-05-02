93500515af4d2150b924b43f8d7c4a9581142b7c
residenti <n.queue.r@gmail.com>
2021-05-02 18:59:20 +0900

    brewでインストールするアイテムを見直した

--- brew.sh
+++ brew.sh
@@ -1,17 +1,12 @@
-brew install openssl
 brew install curl
 brew install fzf
 brew install jq
 brew install bat
-brew install tree
 brew install the_silver_searcher
 brew install vim
 brew install ghq
-brew install rbenv ruby-build
-brew tap heroku/brew && brew install heroku
 
 brew tap homebrew/cask-fonts
 brew cask install font-hack-nerd-font
-
 brew cask install alfred
 brew cask install google-chrome
