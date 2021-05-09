1faefb836eac69d403b5b226ba2dba502c87dc1c
residenti <n.queue.r@gmail.com>
2021-05-09 10:12:58 +0900

    セットアップを時短するため、brewでインストールするものを必要最低限にした

--- brew.sh
+++ brew.sh
@@ -1,12 +1,9 @@
 brew install curl
 brew install fzf
-brew install jq
-brew install bat
 brew install the_silver_searcher
 brew install vim
 brew install ghq
 
 brew tap homebrew/cask-fonts
 brew cask install font-hack-nerd-font
-brew cask install alfred
 brew cask install google-chrome
