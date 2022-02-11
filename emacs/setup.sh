2ef3f14968dbddf6744801fb87c188512643e0a6
residenti <n.queue.r@gmail.com>
2022-02-11 13:32:51 +0900

    rename .spacemacs -> _spacemacs

--- emacs/setup.sh
+++ emacs/setup.sh
@@ -2,4 +2,4 @@ sh ~/dotfiles/setup.sh
 brew install emacs --cask
 rm -rf ~/.emacs.d
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-ln -s ~/dotfiles/emacs/.spacemacs ~/.spacemacs
+ln -s ~/dotfiles/emacs/_spacemacs ~/.spacemacs
