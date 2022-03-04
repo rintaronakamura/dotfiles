36d1072cbb1e7ce79b00eb3af8f51fb4441e6bd9
residenti <n.queue.r@gmail.com>
2022-03-04 18:11:43 +0900

    fixed error enabling flyspell mode.

--- emacs/setup.sh
+++ emacs/setup.sh
@@ -3,3 +3,5 @@ brew install emacs --cask
 rm -rf ~/.emacs.d
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 ln -s ~/dotfiles/emacs/_spacemacs ~/.spacemacs
+brew install aspell
+ln -s ~/dotfiles/emacs/_aspell.conf ~/.aspell.conf
