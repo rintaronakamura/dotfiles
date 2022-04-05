9ec7062df80d26a6c9cbe2236a719968a1b75ccd
residenti <n.queue.r@gmail.com>
2022-04-05 17:21:14 +0900

    setup doom-emacs

--- emacs/setup.sh
+++ emacs/setup.sh
@@ -1,7 +1,10 @@
 sh ~/dotfiles/setup.sh
 brew install emacs --cask
+brew install ripgrep
 rm -rf ~/.emacs.d
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-ln -s ~/dotfiles/emacs/_spacemacs ~/.spacemacs
-brew install aspell
-ln -s ~/dotfiles/emacs/_aspell.conf ~/.aspell.conf
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
+test ! -e ~/.doom.d/ && mkdir -p ~/.doom.d/
+ln -s ~/dotfiles/emacs/_doom.d/config.el ~/.doom.d/config.el
+ln -s ~/dotfiles/emacs/_doom.d/init.el ~/.doom.d/init.el
+ln -s ~/dotfiles/emacs/_doom.d/packages.el ~/.doom.d/packages.el
