9afba270688240547698c3d6d1290d73362cf2de
rintaronakamura <n.queue.r@gmail.com>
2023-04-21 18:12:59 +0900

    Macでターミナル起動時に.bashrcが自動で読み込まれるようにした

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ ln -sf ~/dotfiles/.doom.d/packages.el ~/.doom.d/packages.el
 
 ln -s ~/dotfiles/.zprofile ~/.zprofile
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+# ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 # ln -s ~/dotfiles/.bashrc ~/.bashrc
 
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
